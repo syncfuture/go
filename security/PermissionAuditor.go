5ad69b7befaf023dec3858a0bc7c0162420daadd
Lukiya Chen <lukiya@live.com>
2019-11-11 13:07:32 -0800

    Add PermissionAuditor

---
+++ security/PermissionAuditor.go
@@ -0,0 +1,84 @@
+package security
+
+import (
+	log "github.com/kataras/golog"
+	"github.com/syncfuture/go/auth"
+	"github.com/syncfuture/go/sproto"
+)
+
+type IPermissionAuditor interface {
+	CheckPermission(permissionID string, userRoles int64) bool
+	CheckRoute(area, controller, action string, userRoles int64) bool
+}
+
+type permissionAuditor struct {
+	routePermissionProvider auth.IRoutePermissionProvider
+	routes                  map[string]*sproto.RouteDTO
+	permissions             map[string]*sproto.PermissionDTO
+}
+
+func NewPermissionAuditor(routePermissionProvider auth.IRoutePermissionProvider) IPermissionAuditor {
+	r := new(permissionAuditor)
+	r.routePermissionProvider = routePermissionProvider
+	r.ReloadRoutePermissions()
+	return r
+}
+
+func (x *permissionAuditor) ReloadRoutePermissions() error {
+	r := new(permissionAuditor)
+	var err error
+
+	r.routes, err = x.routePermissionProvider.GetRoutes()
+	if err != nil {
+		return err
+	}
+	r.permissions, err = x.routePermissionProvider.GetPermissions()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (x *permissionAuditor) CheckPermission(permissionID string, userRoles int64) bool {
+	if permission, exists := x.permissions[permissionID]; exists {
+		return checkPermission(permission, userRoles)
+	}
+
+	log.Warnf("permission: %s does not exist", permissionID)
+	return false
+}
+
+func checkPermission(permission *sproto.PermissionDTO, userRoles int64) bool {
+	if permission.IsAllowGuest {
+		return true
+	} else if permission.IsAllowAnyUser {
+		return userRoles > 0
+	} else {
+		return (permission.AllowedRoles & userRoles) > 0
+	}
+}
+
+func (x *permissionAuditor) CheckRoute(area, controller, action string, userRoles int64) bool {
+	key := area + "_" + controller + "_" + action
+
+	route := new(sproto.RouteDTO)
+	var exists bool
+	if route, exists = x.routes[key]; !exists {
+		key = area + "_" + controller + "_"
+		if route, exists = x.routes[key]; !exists {
+			key = area + "__"
+			if route, exists = x.routes[key]; !exists {
+				log.Warnf("route: [%s,%s,%s] does not exist", area, controller, action)
+				return false
+			}
+		}
+	}
+
+	if permission, exists := x.permissions[route.Permission_ID]; exists {
+		return checkPermission(permission, userRoles)
+	}
+
+	log.Warnf("permission: %s does not exist", route.Permission_ID)
+	return false
+}
