06c61915c429fb583cf2d2bf1e30bfbc3ae78ceb
Lukiya Chen <lukiya@live.com>
2019-11-11 11:33:55 -0800

    add security

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/onsi/ginkgo v1.10.3 // indirect
 	github.com/onsi/gomega v1.7.1 // indirect
 	github.com/stretchr/testify v1.4.0
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
 	golang.org/x/sys v0.0.0-20191110163157-d32e6e3b99c4 // indirect
 	google.golang.org/grpc v1.25.1
 )
---
+++ sucurity/security.go
@@ -0,0 +1,38 @@
+package security
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"io"
+
+	log "github.com/kataras/golog"
+	"golang.org/x/crypto/scrypt"
+)
+
+func GeneratePasswordSalt(keyLen int) string {
+	salt := make([]byte, keyLen)
+	_, err := io.ReadFull(rand.Reader, salt)
+	if err != nil {
+		log.Error(err)
+		return err.Error()
+	}
+	saltString := base64.StdEncoding.EncodeToString([]byte(salt))
+	return saltString
+}
+
+func HashPassword(salt, pass string, keyLen int) string {
+	saltBytes, err := base64.StdEncoding.DecodeString(salt)
+	if err != nil {
+		log.Error(err)
+		return err.Error()
+	}
+
+	hash, err := scrypt.Key([]byte(pass), saltBytes, 1<<14, 8, 1, keyLen)
+	if err != nil {
+		log.Error(err)
+		return err.Error()
+	}
+
+	hashString := base64.StdEncoding.EncodeToString([]byte(hash))
+	return hashString
+}
