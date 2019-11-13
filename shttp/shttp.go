15ed45138c5fd92c30555fe38fc4b09ea9404f0a
Lukiya Chen <lukiya@live.com>
2019-11-13 15:03:36 -0800

    enhance shttp

--- shttp/shttp.go
+++ shttp/shttp.go
@@ -2,11 +2,13 @@ package shttp
 
 import (
 	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"sync"
 
-	"github.com/syncfuture/go/json"
+	"github.com/syncfuture/go/surl"
+
 	u "github.com/syncfuture/go/util"
 )
 
@@ -24,38 +26,70 @@ var (
 	}
 )
 
-func CallAPI(client *http.Client, method, apiUrl string, datas ...interface{}) *[]byte {
-	return CallAPIRequest(client, method, apiUrl, nil, datas)
+type APIClient struct {
+	urlProvider surl.IURLProvider
 }
 
-func CallAPIRequest(client *http.Client, method, apiUrl string, configRequest func(*http.Request), body ...interface{}) *[]byte {
+// NewAPIClient create new api client
+// urlProvider is optional, if doesn't provide, it will not render url
+func NewAPIClient(urlProvider surl.IURLProvider) *APIClient {
+	return &APIClient{
+		urlProvider: urlProvider,
+	}
+}
+
+// CallAPI call api
+// bodyObj is optional
+func (x *APIClient) CallAPI(client *http.Client, method, url string, bodyObj interface{}) *[]byte {
+	return x.SendRequest(client, method, url, nil, bodyObj)
+}
+
+func (x *APIClient) SendRequest(client *http.Client, method, url string, configRequest func(*http.Request), bodyObj interface{}) *[]byte {
 	buffer := BytesPool.Get().(*[]byte)
 
 	var err error
 	var request *http.Request
 
-	if len(body) == 1 {
-		var dataJson string
-		dataJson, err = json.Serialize(body[0])
-		if u.LogError(err) {
-			*buffer = []byte(err.Error())
-			return buffer
-		}
-		request, err = http.NewRequest(method, apiUrl, bytes.NewBufferString(dataJson))
+	if x.urlProvider != nil {
+		// 渲染Url
+		url = x.urlProvider.RenderURLCache(url)
+	}
 
+	// 创建Request
+	if bodyObj != nil {
+		var body []byte
+
+		switch v := bodyObj.(type) {
+		case []byte:
+			body = v
+			break
+		case string:
+			body = []byte(v)
+			break
+		default:
+			body, err = json.Marshal(v)
+			if u.LogError(err) {
+				*buffer = []byte(err.Error())
+				return buffer
+			}
+		}
+
+		request, err = http.NewRequest(method, url, bytes.NewBuffer(body))
 	} else {
-		request, err = http.NewRequest(method, apiUrl, nil)
+		request, err = http.NewRequest(method, url, nil)
 	}
 	if u.LogError(err) {
 		*buffer = []byte(err.Error())
 		return buffer
 	}
-	// 设置读取
+
+	// 配置Request
 	request.Header.Set(CNT_TYPE, CNT_TYPE_JSON)
 	if configRequest != nil {
 		configRequest(request)
 	}
 
+	// 发送请求
 	resp, err := client.Do(request)
 	if u.LogError(err) {
 		*buffer = []byte(err.Error())
@@ -63,6 +97,7 @@ func CallAPIRequest(client *http.Client, method, apiUrl string, configRequest fu
 	}
 	defer resp.Body.Close()
 
+	// 读取Response Body
 	*buffer, err = ioutil.ReadAll(resp.Body)
 	if u.LogError(err) {
 		*buffer = []byte(err.Error())
