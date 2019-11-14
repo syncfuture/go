8dbb557ae63e5a92ce3342ce4eb6e9470e1f36d7
Lukiya Chen <lukiya@live.com>
2019-11-14 09:37:46 -0800

    update timestamp

--- sproto/timestamp/jsonpb.go
+++
@@ -1,5 +0,0 @@
-package timestamp
-
-// XXX_WellKnownType allows this non-Google timestamp protobuf to interact properly with jsonpb
-// and other libraries as it is was the standard WKT timestamp.
-func (*Timestamp) XXX_WellKnownType() string { return "Timestamp" }
--- sproto/timestamp/timestamp.ext.go
+++ sproto/timestamp/timestamp.ext.go
@@ -23,3 +23,7 @@ func (m *Timestamp) Scan(value interface{}) error {
 func (m Timestamp) Value() (driver.Value, error) {
 	return m.Time()
 }
+
+// XXX_WellKnownType allows this non-Google timestamp protobuf to interact properly with jsonpb
+// and other libraries as it is was the standard WKT timestamp.
+func (*Timestamp) XXX_WellKnownType() string { return "Timestamp" }
