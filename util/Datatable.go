998b300cade612922851afcaaf926d6e20fc5a9b
Lukiya <lukiya.chen@syncsoftinc.com>
2019-11-22 15:02:41 -0800

    add dt

---
+++ util/Datatable.go
@@ -0,0 +1,18 @@
+package u
+
+type DataTableDTO struct {
+	Draw    int    `url:"draw" form:"draw" json:"draw"`
+	Start   int    `url:"start" form:"start" json:"start"`
+	Length  int    `url:"length" form:"length" json:"length"`
+	OrderBy int    `url:"order[0][column]" form:"order[0][column]" json:"order[0][column]"`
+	Sort    string `url:"order[0][dir]" form:"order[0][dir]" json:"order[0][dir]"`
+	Keyword string `url:"search[value]" form:"search[value]" json:"search[value]"`
+}
+
+func (x *DataTableDTO) GetPageIndex() int32 {
+	if x.Length <= 0 {
+		return 1
+	} else {
+		return int32((x.Start / x.Length) + 1)
+	}
+}
