07324a3ac5b01f3aa8886569eb858a3c313cb719
Lukiya <lukiya.chen@syncsoftinc.com>
2019-11-21 16:30:33 -0800

    add task package

---
+++ task/batchScheduler.go
@@ -0,0 +1,75 @@
+package task
+
+import (
+	"math"
+	"reflect"
+	"sync"
+	"time"
+
+	log "github.com/kataras/golog"
+)
+
+type batchScheduler struct {
+	batchSize int
+	// Intervel intervel milliseconds per batch
+	intervalMS int
+	action     func(int, interface{})
+}
+
+func NewBatchScheduler(batchSize, intervalMS int, action func(int, interface{})) *batchScheduler {
+	return &batchScheduler{
+		batchSize:  batchSize,
+		intervalMS: intervalMS,
+		action:     action,
+	}
+}
+
+func (x *batchScheduler) Run(slicePtr interface{}) {
+	v := reflect.ValueOf(slicePtr)
+	if v.Kind() != reflect.Ptr {
+		log.Fatal("slicePtr must be a slice pointer")
+	}
+
+	s := v.Elem()
+	if s.Kind() != reflect.Slice {
+		log.Fatal("slicePtr must be a slice pointer")
+	}
+
+	if x.batchSize <= 0 {
+		log.Fatal("batch size must be positive number.")
+	}
+
+	if x.intervalMS < 0 {
+		log.Fatal("interval must be 0 or positive number.")
+	}
+
+	totalCount := s.Len()
+	// 总页数
+	totalPages := int(math.Ceil(float64(totalCount) / float64(x.batchSize)))
+
+	wg := &sync.WaitGroup{}
+	for pageIndex := 0; pageIndex < totalPages; pageIndex++ {
+		if pageIndex == totalPages-1 {
+			wg.Add(totalCount % x.batchSize) // 余下的个数
+		} else {
+			wg.Add(x.batchSize) // 页数
+		}
+
+		for pageLoopIndex := 0; pageLoopIndex < x.batchSize; pageLoopIndex++ {
+			itemIndex := pageIndex*x.batchSize + pageLoopIndex
+			if itemIndex >= totalCount {
+				break
+			}
+			go func(i int) {
+				defer wg.Done()
+				x.action(i, s.Index(i).Interface())
+			}(itemIndex)
+		}
+
+		wg.Wait()
+
+		if x.intervalMS > 0 && pageIndex < totalPages-1 {
+			time.Sleep(time.Duration(x.intervalMS) * time.Millisecond)
+		}
+	}
+}
---
+++ task/flowScheduler.go
@@ -0,0 +1,69 @@
+package task
+
+import (
+	"reflect"
+	"sync"
+
+	log "github.com/kataras/golog"
+)
+
+type flowScheduler struct {
+	maxConcurrent int
+}
+
+// NewFlowScheduler create a flow scheduler
+func NewFlowScheduler(maxConcurrent int) *flowScheduler {
+	r := new(flowScheduler)
+	r.maxConcurrent = maxConcurrent
+	return r
+}
+
+// Run run with slice, make each action under scheduler control
+func (x *flowScheduler) SliceRun(slicePtr interface{}, action func(i int, v interface{})) {
+	v := reflect.ValueOf(slicePtr)
+	if v.Kind() != reflect.Ptr {
+		log.Fatal("slicePtr must be a slice pointer")
+	}
+
+	s := v.Elem()
+	if s.Kind() != reflect.Slice {
+		log.Fatal("slicePtr must be a slice pointer")
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(s.Len())
+	ch := make(chan byte, x.maxConcurrent)
+
+	for i := 0; i < s.Len(); i++ {
+		ch <- 0
+		go func(a int) {
+			defer func() {
+				<-ch
+				wg.Done()
+			}()
+			action(a, s.Index(a))
+		}(i)
+	}
+
+	wg.Wait()
+}
+
+// Run run under scheduler control
+func (x *flowScheduler) Run(repeatTime int, action func(i int)) {
+	wg := sync.WaitGroup{}
+	wg.Add(repeatTime)
+	ch := make(chan byte, x.maxConcurrent)
+
+	for i := 0; i < repeatTime; i++ {
+		ch <- 0
+		go func(a int) {
+			defer func() {
+				<-ch
+				wg.Done()
+			}()
+			action(a)
+		}(i)
+	}
+
+	wg.Wait()
+}
