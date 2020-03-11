a5cbe8b7fac402d7ce0b0401e122dff1cd71ed4c
Lukiya <lukiya.chen@syncsoftinc.com>
2020-03-11 11:23:32 -0700

    add foreach

--- task/Parallel.go
+++ task/Parallel.go
@@ -1,6 +1,11 @@
 package task
 
-import "github.com/syncfuture/go/dto"
+import (
+	"reflect"
+
+	log "github.com/kataras/golog"
+	"github.com/syncfuture/go/dto"
+)
 
 type parallel struct{}
 
@@ -15,12 +20,40 @@ func (x *parallel) Invoke(actions []func(chan *dto.ChannelResultDTO)) (r []*dto.
 		chs[i] = make(chan *dto.ChannelResultDTO)
 	}
 
-	r = make([]*dto.ChannelResultDTO, actionCount)
-
 	for i, action := range actions {
 		go action(chs[i])
 	}
 
+	r = make([]*dto.ChannelResultDTO, actionCount)
+	for i, ch := range chs {
+		r[i] = <-ch
+	}
+
+	return
+}
+
+func (x *parallel) ForEach(slicePtr interface{}, action func(chan *dto.ChannelResultDTO, int, interface{})) (r []*dto.ChannelResultDTO) {
+	v := reflect.ValueOf(slicePtr)
+	if v.Kind() != reflect.Ptr {
+		log.Fatal("slicePtr must be a slice pointer")
+	}
+
+	s := v.Elem()
+	if s.Kind() != reflect.Slice {
+		log.Fatal("slicePtr must be a slice pointer")
+	}
+
+	actionCount := s.Len()
+	chs := make([]chan *dto.ChannelResultDTO, actionCount)
+	for i := 0; i < actionCount; i++ {
+		chs[i] = make(chan *dto.ChannelResultDTO)
+	}
+
+	for i := 0; i < s.Len(); i++ {
+		go action(chs[i], i, s.Index(i).Interface())
+	}
+
+	r = make([]*dto.ChannelResultDTO, actionCount)
 	for i, ch := range chs {
 		r[i] = <-ch
 	}
