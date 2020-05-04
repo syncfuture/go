d3534610fb0d5310c74251e4b588d714d449c033
Lukiya <lukiya@outlook.com>
2020-05-04 15:33:09 -0700

    update

--- sarray/utils.go
+++ sarray/utils.go
@@ -1,17 +1,16 @@
 package sarray
 
-func HasStr(array []string, in string) bool {
-	for _, item := range array {
+func HasStr(source []string, in string) bool {
+	for _, item := range source {
 		if item == in {
 			return true
 		}
 	}
 	return false
 }
-
-func HasAnyStr(array []string, in []string) bool {
-	for _, item := range array {
-		for _, v := range in {
+func HasAnyStr(source []string, in []string) bool {
+	for _, v := range in {
+		for _, item := range source {
 			if item == v {
 				return true
 			}
@@ -19,19 +18,34 @@ func HasAnyStr(array []string, in []string) bool {
 	}
 	return false
 }
+func HasAllStr(source []string, in []string) bool {
+	for _, v := range in {
+		var found bool
+		for _, item := range source {
+			if item == v {
+				found = true
+				break
+			}
+		}
 
-func HasInt(array []int, in int) bool {
-	for _, item := range array {
+		if !found {
+			return false
+		}
+	}
+	return true
+}
+
+func HasInt(source []int, in int) bool {
+	for _, item := range source {
 		if item == in {
 			return true
 		}
 	}
 	return false
 }
-
-func HasAnyInt(array []int, in []int) bool {
-	for _, item := range array {
-		for _, v := range in {
+func HasAnyInt(source []int, in []int) bool {
+	for _, v := range in {
+		for _, item := range source {
 			if item == v {
 				return true
 			}
@@ -39,19 +53,34 @@ func HasAnyInt(array []int, in []int) bool {
 	}
 	return false
 }
+func HasAllInt(source []int, in []int) bool {
+	for _, v := range in {
+		var found bool
+		for _, item := range source {
+			if item == v {
+				found = true
+				break
+			}
+		}
 
-func HasInt32(array []int32, in int32) bool {
-	for _, item := range array {
+		if !found {
+			return false
+		}
+	}
+	return true
+}
+
+func HasInt32(source []int32, in int32) bool {
+	for _, item := range source {
 		if item == in {
 			return true
 		}
 	}
 	return false
 }
-
-func HasAnyInt32(array []int32, in []int32) bool {
-	for _, item := range array {
-		for _, v := range in {
+func HasAnyInt32(source []int32, in []int32) bool {
+	for _, v := range in {
+		for _, item := range source {
 			if item == v {
 				return true
 			}
@@ -59,19 +88,34 @@ func HasAnyInt32(array []int32, in []int32) bool {
 	}
 	return false
 }
+func HasAllInt32(source []int32, in []int32) bool {
+	for _, v := range in {
+		var found bool
+		for _, item := range source {
+			if item == v {
+				found = true
+				break
+			}
+		}
 
-func HasInt64(array []int64, in int64) bool {
-	for _, item := range array {
+		if !found {
+			return false
+		}
+	}
+	return true
+}
+
+func HasInt64(source []int64, in int64) bool {
+	for _, item := range source {
 		if item == in {
 			return true
 		}
 	}
 	return false
 }
-
-func HasAnyInt64(array []int64, in []int64) bool {
-	for _, item := range array {
-		for _, v := range in {
+func HasAnyInt64(source []int64, in []int64) bool {
+	for _, v := range in {
+		for _, item := range source {
 			if item == v {
 				return true
 			}
@@ -79,19 +123,34 @@ func HasAnyInt64(array []int64, in []int64) bool {
 	}
 	return false
 }
+func HasAllInt64(source []int64, in []int64) bool {
+	for _, v := range in {
+		var found bool
+		for _, item := range source {
+			if item == v {
+				found = true
+				break
+			}
+		}
 
-func HasFloat32(array []float32, in float32) bool {
-	for _, item := range array {
+		if !found {
+			return false
+		}
+	}
+	return true
+}
+
+func HasFloat32(source []float32, in float32) bool {
+	for _, item := range source {
 		if item == in {
 			return true
 		}
 	}
 	return false
 }
-
-func HasAnyFloat32(array []float32, in []float32) bool {
-	for _, item := range array {
-		for _, v := range in {
+func HasAnyFloat32(source []float32, in []float32) bool {
+	for _, v := range in {
+		for _, item := range source {
 			if item == v {
 				return true
 			}
@@ -99,23 +158,54 @@ func HasAnyFloat32(array []float32, in []float32) bool {
 	}
 	return false
 }
+func HasAllFloat32(source []float32, in []float32) bool {
+	for _, v := range in {
+		var found bool
+		for _, item := range source {
+			if item == v {
+				found = true
+				break
+			}
+		}
 
-func HasFloat64(array []float64, in float64) bool {
-	for _, item := range array {
+		if !found {
+			return false
+		}
+	}
+	return true
+}
+
+func HasFloat64(source []float64, in float64) bool {
+	for _, item := range source {
 		if item == in {
 			return true
 		}
 	}
 	return false
 }
-
-func HasAnyFloat64(array []float64, in []float64) bool {
-	for _, item := range array {
-		for _, v := range in {
+func HasAnyFloat64(source []float64, in []float64) bool {
+	for _, v := range in {
+		for _, item := range source {
 			if item == v {
 				return true
 			}
 		}
 	}
 	return false
+}
+func HasAllFloat64(source []float64, in []float64) bool {
+	for _, v := range in {
+		var found bool
+		for _, item := range source {
+			if item == v {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			return false
+		}
+	}
+	return true
 }
