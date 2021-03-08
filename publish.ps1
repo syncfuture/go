f52ffb7c7b9548c8b3b9e1b42c146958a0fdaef0
Lukiya Chen <lukiya@outlook.com>
2021-03-08 15:15:08 -0800

    update

--- publish.ps1
+++ publish.ps1
@@ -1,9 +1,5 @@
 #git tag | foreach-object -process { git push origin --delete $_ }
 #git tag | foreach-object -process { git tag -d $_ }
-<<<<<<< HEAD
-git tag v1.4.1
-=======
-git tag v1.4.0
->>>>>>> v1
+git tag v1.4.2
 git push
 git push --tags
