af36886aeb5507b9ac392d68b0f5ed9f6a48ea9e
Lukiya Chen <lukiya@outlook.com>
2025-02-06 14:13:37 -0800

    u

--- publish.ps1
+++ publish.ps1
@@ -1,5 +1,5 @@
 #git tag | foreach-object -process { git push origin --delete $_ }
 #git tag | foreach-object -process { git tag -d $_ }
 git push
-git tag v1.18.1
+git tag v1.18.2
 git push --tags
--- ssecurity/PermissionAuditor.go
+++ ssecurity/PermissionAuditor.go
@@ -66,8 +66,8 @@ func (x *permissionAuditor) CheckPermissionWithLevel(permissionID string, userRo
 }
 
 func checkPermission(permission *sproto.PermissionDTO, userRoles int64, userLevel int32, userScopes []string) bool {
-	// 没有scope的权限，直接通过，或者用户没有scope，或者权限的scope不是用户的scope的子集
-	if len(permission.Scopes) == 0 || len(userScopes) == 0 || !sslice.HasAllStr(userScopes, permission.Scopes) {
+	// 权限设置限制了scope，但是 (用户没有scope，或者权限的要求的scope不是用户的scope的子集)
+	if len(permission.Scopes) > 0 && (len(userScopes) == 0 || !sslice.HasAllStr(userScopes, permission.Scopes)) {
 		return false
 	}
 
