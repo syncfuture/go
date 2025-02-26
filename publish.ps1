3da8ecad6e58252e36154918fdfeec946d9815c5
Lukiya Chen <lukiya@outlook.com>
2025-02-26 08:57:55 -0800

    u

--- publish.ps1
+++ publish.ps1
@@ -1,5 +1,5 @@
 #git tag | foreach-object -process { git push origin --delete $_ }
 #git tag | foreach-object -process { git tag -d $_ }
 git push
-git tag v1.19.0
+git tag v1.20.0
 git push --tags
--- stime/stime.go
+++ stime/stime.go
@@ -11,3 +11,19 @@ func UTCNowUnixMS() int64 {
 func MSToUTCTime(ms int64) time.Time {
 	return time.UnixMilli(ms).UTC()
 }
+
+func GetWeekStartEnd(year, week int) (time.Time, time.Time) {
+	// Get the first day of the year
+	firstDay := time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC)
+	// Calculate the weekday of the first day
+	weekday := int(firstDay.Weekday())
+	if weekday == 0 {
+		weekday = 7 // Set Sunday to 7
+	}
+
+	// Calculate the first day of the given week (ISO weeks start on Monday)
+	startDate := firstDay.AddDate(0, 0, (week-1)*7-(weekday-1))
+	endDate := startDate.AddDate(0, 0, 6) // Calculate Sunday
+
+	return startDate, endDate
+}
